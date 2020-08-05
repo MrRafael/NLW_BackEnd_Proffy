e2865788d31e1dbbb485af4b00850ce9c20c2835
Rafael <mrrafaeldeoliveira@gmail.com>
2020-08-05 19:11:28 -0300

    Ajustes de escrita feito

--- src/controller/ClassController.ts
+++ src/controller/ClassController.ts
@@ -13,7 +13,7 @@ export default class ClassController {
     async index(request: Request, response: Response) {
         const filter = request.query;
 
-        if (!filter.weekday || !filter.subject || !filter.time) {
+        if (!filter.week_day || !filter.subject || !filter.time) {
             return response.status(400).json({
                 error: "Filtros não encontrados"
             });
@@ -24,21 +24,27 @@ export default class ClassController {
         const time = filter.time as string;
 
         const timeInMinute = convertHourToMinutes(time as string);
+        try {
 
-        const classes = await db('class')
-            .whereExists(function () {
-                this.select('class_schedule.*')
-                    .from('class_schedule')
-                    .whereRaw('`class_schedule`.`class_id` = `class`.``id')
-                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-                    .whereRaw('`class_schedule`.`from` <= ??', [timeInMinute])
-                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinute])
-            })
-            .where('class.subject', '=', subject as string)
-            .join('user', 'class.user_id', '=', 'user_id')
-            .select(['class.*', 'user.*']);
+            const classes = await db('class')
+                .whereExists(function () {
+                    this.select('class_schedule.*')
+                        .from('class_schedule')
+                        .whereRaw('`class_schedule`.`class_id` = `class`.`id`')
+                        .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                        .whereRaw('`class_schedule`.`from` <= ??', [timeInMinute])
+                        .whereRaw('`class_schedule`.`to` > ??', [timeInMinute])
+                })
+                .where('class.subject', '=', subject as string)
+                .join('user', 'class.user_id', '=', 'user_id')
+                .select(['class.*', 'user.*']);
 
-        return response.json(classes);
+            return response.json(classes);
+        } catch (error) {
+            console.log(error);
+
+        }
+
     }
 
     async create(request: Request, response: Response) {
@@ -78,12 +84,14 @@ export default class ClassController {
                 };
             });
 
-            await trx('schedule').insert(scheduleToSave);
+            await trx('class_schedule').insert(scheduleToSave);
 
             await trx.commit();
 
             return response.status(201).send();
         } catch (erro) {
+            console.log(erro);
+
             await trx.rollback();
             return response.status(500).json({
                 error: "Erro inesperado"
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import ClassController from './controller/ClassController';
+import ConnectionController from './controller/ConnectionController';
 
 
 const routes = express.Router();
 const classController = new ClassController();
+const connectionController = new ConnectionController();
 
 routes.get('/class', classController.index);
 routes.post('/class', classController.create);
 
-routes.get('/connection', classController.index);
-routes.post('/connection', classController.create);
+routes.get('/connection', connectionController.index);
+routes.post('/connection', connectionController.create);
 
 export default routes;
